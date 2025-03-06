b2a0963ca637752b2fbd9d03bfdb68b6f7443a25
Lloyd Languido <79763710+Syrulli@users.noreply.github.com>
2025-03-07 01:33:04 +0800

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    appDir: true, // If using the app router
+  },
+  reactStrictMode: true,
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
 };
 
 export default nextConfig;
